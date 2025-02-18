f4f4855f6c2e7320f9a17aa428571c6099fb92d6
Om Mehta <ommehta16@icloud.com>
2025-02-18 14:47:02 -0500

    fix test flight code. it works now i think?

--- testflight.py
+++ testflight.py
@@ -27,7 +27,8 @@ connection = mavutil.mavlink_connection(CONNECTIONPORT,baud=57600)
 
 connection.wait_heartbeat()
 
-all_message = open("LOG.txt","a+")
+all_message = open("LOG.txt","+a")
+vlt_log = open("VOLTAGE", "+ab")
 
 start = time.time()
 
@@ -39,10 +40,8 @@ wanted = [
     mavutil.mavlink.ATTITUDE,
     mavutil.mavlink.SCALED_PRESSURE,
     mavutil.mavlink.GLOBAL_POSITION_INT,
-    mavutil.mavlink.SYS_STATUS,
+    # mavutil.mavlink.SYS_STATUS,
 ]
-
-
 
 for _ in range(10):
     
@@ -56,7 +55,7 @@ for _ in range(10):
                 mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,  # ID of command to send
                 0,  # Confirmation
                 cmd,  # param1: Message ID to be streamed
-                1000000, # param2: Interval in MICROseconds
+                1e6, # param2: Interval in MICROseconds
                 0,       # param3 (unused)
                 0,       # param4 (unused)
                 0,       # param5 (unused)
@@ -65,13 +64,68 @@ for _ in range(10):
         )
 
         connection.mav.send(request)
+    
+for _ in range(10): # bomb with requests for system status at 100Hz
+    request = connection.mav.command_long_encode(
+            connection.target_system,  # Target system ID
+            connection.target_component,  # Target component ID
+            mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,  # ID of command to send
+            0,  # Confirmation
+            mavutil.mavlink.SYS_STATUS,  # param1: Message ID to be streamed
+            1e4, # param2: Interval in MICROseconds
+            0,       # param3 (unused)
+            0,       # param4 (unused)
+            0,       # param5 (unused)
+            0,       # param5 (unused)
+            0        # param6 (unused)
+    )
+
+    connection.mav.send(request)
+
+volts = 0
+
+TIME_BASE = 0
 
 while time.time()-start < TWO_HOURS:
     msg = connection.recv_msg()
-
     if not msg: continue
-    
+    if TIME_BASE == 0: TIME_BASE = msg.timestamp
     # log the messages
     # AHDKFHDASFHJGDSHJAGFHJDSJAAAH
+
+    if msg.get_type() == mavutil.mavlink.SYS_STATUS:
+        try:
+            tmp = msg.voltage_battery
+
+            if (abs(tmp-volts) >= 1):
+                volts = tmp
+                curr_time = msg.timestamp-TIME_BASE
+                vlt_log.write(curr_time.to_bytes(64,'big'))
+                vlt_log.write(volts.to_bytes(64,'big'))
+        except: pass
     
     all_message.write(msg + "\n\n--------------------\n\n")
+
+def readVolts(fp: str = "VOLTAGE") -> dict:
+    '''
+    Reads voltage values from the given file (`VOLTAGE` if none specified)
+
+    Outputs a dict that maps from int (timestamp = microseconds since start) to int (voltage = mV)
+    '''
+    log = open(fp,'rb')
+
+    MAX_SAMPLES = int(7.2e6) # this is if we had 1000Hz for 2 hours... not happening lol
+    samples = 0
+
+    voltsAt = {}
+    
+    while samples < MAX_SAMPLES:
+        samples+=1
+        tim = log.read(64)
+        if tim is None: break
+        vlt = log.read(64)
+        if vlt is None: break
+
+        try: voltsAt[int.from_bytes(tim,'big')] = int.from_bytes(vlt,'big')
+        except: pass
+    return voltsAt
